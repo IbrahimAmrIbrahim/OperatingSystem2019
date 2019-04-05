0b3fd8220dba6a0c4850442fefa67eb2a3b89a55
ahmed hessuin. <ahmed_1k997@yahoo.com>
2019-04-06 00:52:47 +0200

    insert and sort done

--- Scheduler/src/schedulerAlgorithm/RoundRobin.java
+++ Scheduler/src/schedulerAlgorithm/RoundRobin.java
@@ -18,43 +18,45 @@ public class RoundRobin extends Queue implements ReadyQueue {
     @Override
     public void insert(PCB newPCB) {
 
-        //        waiting_time+=newPCB.getBurstTime(); // take the waiting time 
-        // my qq must be special
-        // as if process newpcb period is biger than the qq period it will be inside the qq agian as new processs
-        // so the system i should go with is insert first time  
-        // p1 p2 p3 p4 then p1 p2 p3 p4 again
-        // this is special case of the specials
-        // i need 
-        //process -> brust time and arrival time .
-        // i need first qq then a second qq 
-        // i need to know when we call insert .
-        
-        
         //==============================//
         
         
         // if the time is less than the RR_TIME then just add it in the qq 
-        if (newPCB.getBurstTime() > 0) {
+        if (newPCB.getBurstTime() <= RR_time) {
             enqueue(newPCB);
             node_number++;
         } 
         
-        
-        //-------------------------------------------------------//
-        // the time is bigger than the qq time, then i will suffer 
-        // i need to add it again so i need to change in the schedual itself 
-        // chanign it from p1 p2 p3 p4 to be p1 p2 p3 p4 p1 etc
+        //======================================================//
         
         
         else {
+            System.out.println("flag 1");
             // while the burst time bigger than zero
             while (newPCB.getBurstTime() > 0) {
                 int new_pcb_time = newPCB.getBurstTime();
+                PCB next_new = new PCB(false) ;// fix it
+               
+                int put_in;
+                //======================================//
+                if (newPCB.getBurstTime() > RR_time)
+                {
+                    put_in=RR_time;
+                }
+                else 
+                {
+                    put_in=newPCB.getBurstTime();
+                }
                 //=================================================//
-
-                newPCB.setBurstTime(RR_time);// change the time to be as qq time
-                enqueue(newPCB);
+             
                 
+                newPCB.setBurstTime(put_in);// change the time to be as qq time
+                //next_new start copy here
+                next_new.copy(newPCB);
+               
+                enqueue(next_new);
+              
+                      
                 node_number++;
                 //====================================//
                 //change the burst time from x to x - RR_time 
@@ -62,18 +64,20 @@ public class RoundRobin extends Queue implements ReadyQueue {
                 //============================================//
                 newPCB.setBurstTime(new_pcb_time - RR_time);
                 newPCB.setArrivalTime(newPCB.getArrivalTime() + RR_time);
-
+                
                 //===========================================//
                 // insert it again  
             }
         }
-
+         
+sort(0);
+printQueue();
     }
 
     // insert done 
     @Override
     public void DrawGanttChart(SchedulerSimulationController ctrl) {
-        sort(0);
+        
         
     }
 
@@ -81,32 +85,44 @@ public class RoundRobin extends Queue implements ReadyQueue {
    
 
     public void sort(int mode) {
-
+       
         if (mode == 0)// arive time 
         {
-
+         
             // bubble sort with node 
-            Node temp;
+            Node temp ;
             for (int i = 0; i < node_number; i++) {
                 temp = head;
-
-                while (temp.getNext() != null) {
-                    if (temp.getPcb().getArrivalTime() >= temp.getNext().getPcb().getArrivalTime()) {
+                 Node current;
+                 Node next;
+                 current=temp;
+                 next=temp.getNext();
+                 PCB my_pcb = new PCB(false);
+                while (current.getNext() != null) {
+                   
+                    if (current.getPcb().getArrivalTime() > next.getPcb().getArrivalTime()) {
                         //===============//
-                        Node dummy;
-                        dummy = temp.getNext();// dummy = next of temp
+                      
+                       
+                       my_pcb.copy(next.getPcb());
+                       
+                       
                         //==================//
 
                         //=====================================//
                         // temp ->next->next of the next
-                        temp.getNext().setNext(temp);// let the next point at temp      temp ->next and temp<-next 
-                        temp.setNext(dummy.getNext());// let temp point at next of the next  next->temp->next of the next 
-                        
+                        next.getPcb().copy(current.getPcb());// let the next point at temp      temp ->next and temp<-next 
+                        current.getPcb().copy(my_pcb);
+                        current=next;
+                       
+                          
                         //=====================================//
                     }
                     else 
                     {
-                        temp=temp.getNext();
+                        
+                        current=next;
+                        next=current.getNext();
                     }
                 }
 
