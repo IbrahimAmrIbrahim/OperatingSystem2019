cb5d7cd1851e542aa84c2ed3691facc5ff23eaf5
Ibrahim Amr <ibrahem.amr.eid@gmail.com>
2019-04-05 21:52:06 +0200

    RoundRobin Test

--- Scheduler/src/scheduler/SchedulerSimulationController.java
+++ Scheduler/src/scheduler/SchedulerSimulationController.java
@@ -199,7 +199,7 @@ public class SchedulerSimulationController implements Initializable {
         if (newProcess == true) {
             /* Add process */
             insertMethodCall(processtoAdd);
-            Priority_NonPreemptive_FCFS_ProcessQueue.printQueue();
+            RoundRobin_ProcessQueue.printQueue();
         }
     }
 
