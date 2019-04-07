80fcefd58fd5e3a70290c62a3c476254647a8bd5
Ahmed-Abdalla-2020 <36240815+Ahmed-Abdalla-2020@users.noreply.github.com>
2019-04-07 19:05:13 +0200

    Update SJF_NonPreemptive_FCFS.java
    part of code

--- Scheduler/src/schedulerAlgorithm/SJF_NonPreemptive_FCFS.java
+++ Scheduler/src/schedulerAlgorithm/SJF_NonPreemptive_FCFS.java
@@ -1,5 +1,6 @@
 package schedulerAlgorithm;
 
+import dataStructure.Node;
 import dataStructure.PCB;
 import dataStructure.Queue;
 import scheduler.SchedulerSimulationController;
@@ -8,6 +9,32 @@ public class SJF_NonPreemptive_FCFS extends Queue implements ReadyQueue {
 
     @Override
     public void insert(PCB newPCB) {
+        Node newNo = new Node(newPCB);
+        
+        if (head == null) {
+            head = newNo;
+            tail = newNo;
+        }
+        
+        else if(head==tail){
+            if(head.getPcb().getArrivalTime() == newNo.getPcb().getArrivalTime()){
+                if(head.getPcb().getBurstTime() <= newNo.getPcb().getBurstTime()){
+                    enqueue(newPCB);
+                }
+                else{
+                    newNo.setNext(head);
+                    head=newNo;
+                }
+            }
+            else if(head.getPcb().getArrivalTime() < newNo.getPcb().getArrivalTime()){
+                enqueue(newPCB);
+            }
+            else{
+                newNo.setNext(head);
+                head=newNo;
+            }
+        }
+        
     }
 
     @Override
