d688e364367dce232847dceb5eb717afb4cfe5b4
ahmed hessuin. <ahmed_1k997@yahoo.com>
2019-04-10 14:04:52 +0200

    final before tests

--- Scheduler/src/schedulerAlgorithm/RoundRobin.java
+++ Scheduler/src/schedulerAlgorithm/RoundRobin.java
@@ -3,7 +3,7 @@ package schedulerAlgorithm;
 import dataStructure.PCB;
 import dataStructure.Queue;
 import dataStructure.Node;
- 
+
 import javafx.scene.paint.Color;
 import scheduler.SchedulerSimulationController;
 
@@ -15,6 +15,8 @@ public class RoundRobin extends Queue implements ReadyQueue {
     private Queue my_queue = new Queue();
     private int last_arrival_time = 0;
     private int run_time = 0;
+    private float turn_around_avr = 0;
+    private float waiting_avr = 0;
     // run time must be a factor of 10 like 0 10 20 30 etc
 
     private int waiting_time = 0;
@@ -26,39 +28,39 @@ public class RoundRobin extends Queue implements ReadyQueue {
     public void insert(PCB newPCB) {
 
         enqueue(newPCB);
-        
 
     }
 //======================================================//
 
-    public void reinsert(int slice)
-    {
+    public void reinsert(int slice) {
         Node current;
-        current=head;
-         RR_time=slice;
-       while(current!=null)
-        {
-          
-          insert2(current.getPcb());
-           current=current.getNext();
-            
+        current = head;
+        RR_time = slice;
+        while (current != null) {
+
+            insert2(current.getPcb());
+            current = current.getNext();
+
         }
     }
+
     // insert done 
     @Override
     public void DrawGanttChart(SchedulerSimulationController ctrl) {
-       
+
         int slice;
-        slice= ctrl.getTimeSlice();
+        slice = ctrl.getTimeSlice();
         reinsert(slice);
-         Node temp2 = my_queue.getHead();
+        Node temp2 = my_queue.getHead();
         sort(0);
+        sort_2(0);
+
+        time_confg();
         int start_time = 0;
         for (int i = 0; i < node_number; i++) {
             //fixed area //
 
             //========================================//
-           
             if (temp2.getPcb().getStartofExec() > start_time) {
                 ctrl.drawIdleProcess(temp2.getPcb().getStartofExec() - start_time);
 
@@ -67,23 +69,26 @@ public class RoundRobin extends Queue implements ReadyQueue {
             start_time = temp2.getPcb().getEndofExec();
             temp2 = temp2.getNext();
         }
-       
-        ctrl.writeAvgWaitingTime(time(3));
-        ctrl.writeAvgTurnarroundTime(time(2));
+
+        ctrl.writeAvgWaitingTime(waiting_avr);
+        ctrl.writeAvgTurnarroundTime(turn_around_avr);
     }
 
     public void insert2(PCB newPCB) {
         PCB pcb_dumy = new PCB(false);
+        int start = pcb_dumy.getArrivalTime();
+        int end = pcb_dumy.getArrivalTime() + RR_time;
         pcb_dumy.copy(newPCB);
         //==============================//
         // if the time is less than the RR_TIME then just add it in the qq 
         if (pcb_dumy.getBurstTime() <= RR_time) {
+            pcb_dumy.setStartofExec(pcb_dumy.getArrivalTime());
+            pcb_dumy.setEndofExec(pcb_dumy.getArrivalTime() + RR_time);
             my_queue.enqueue(pcb_dumy);
             node_number++;
 
         } //======================================================//
         else {
-            System.out.println("flag 1");
 
             PCB new_pcb = new PCB(false);
             new_pcb.copy(pcb_dumy);
@@ -107,6 +112,9 @@ public class RoundRobin extends Queue implements ReadyQueue {
                 //next_new start copy here
                 next_new.copy(new_pcb);
 
+                //=========================================================//
+                next_new.setStartofExec(start);
+                next_new.setEndofExec(end);
                 my_queue.enqueue(next_new);
 
                 node_number++;
@@ -114,78 +122,163 @@ public class RoundRobin extends Queue implements ReadyQueue {
                 //change the burst time from x to x - RR_time 
                 // example from 15 to 15  - 10 = 5
                 //============================================//
-                new_pcb.setPriority(-1);
+                new_pcb.setPriority(new_pcb.getPriority() - 1);
                 new_pcb.setBurstTime(new_pcb_time - RR_time);
+
                 new_pcb.setArrivalTime(new_pcb.getArrivalTime() + RR_time);
+
+                start = start + RR_time;
+                end = end + RR_time;
 
                 //===========================================//
                 // insert it again  
             }
 
         }
-        System.out.println("---------------------");
-        newPCB.printPCB();
-        System.out.println("the fucken pcb above ");
 
-        printQueue();
-        time(0);
     }
 
     public void sort(int mode) {
+        for (int i = 0; i < node_number; i++) {
+            Node current = my_queue.getHead();
+            Node next = current.getNext();
+            PCB drifter = new PCB(false);
 
-        if (mode == 0)// arive time 
-        {
+            while (next != null) {
+                int start_current = current.getPcb().getStartofExec();
+                int end_current = current.getPcb().getStartofExec() + RR_time;
+                int start_next = next.getPcb().getStartofExec();
+                int end_next = next.getPcb().getStartofExec() + RR_time;
 
-            // bubble sort with node 
-            Node temp;
-            int iterator = node_number;
-            boolean flip = true;
-            for (int i = 0; i < iterator && flip; i++, iterator--) {
-                temp =my_queue.getHead();
-                flip = false;
-                Node current;
-                Node next;
-                current = temp;
-                next = temp.getNext();
-                PCB my_pcb = new PCB(false);
-                while (current.getNext() != null) {
+                // iterate till the end 
+                int current_arrival = current.getPcb().getArrivalTime();
+                int next_arrival = next.getPcb().getArrivalTime();
 
-                    if (current.getPcb().getArrivalTime() > next.getPcb().getArrivalTime()
-                            || (current.getPcb().getArrivalTime() == next.getPcb().getArrivalTime()
-                            && (current.getPcb().getPID() < next.getPcb().getPID()
-                            && current.getPcb().getPriority() < next.getPcb().getPriority()))) {
-                        //===============//
-                        flip = true;
-                        my_pcb.copy(next.getPcb());
+                //first case normal case done 
+                if (current_arrival > next_arrival) {
 
-                        //==================//
-                        //=====================================//
-                        next.getPcb().copy(current.getPcb());// let the next point at temp      temp ->next and temp<-next 
-                        current.getPcb().copy(my_pcb);
+                    drifter.copy(current.getPcb());
+                    current.getPcb().copy(next.getPcb());
 
-                        //==============================//
-                        //================================//
-                        current = next;
+                    current.getPcb().setStartofExec(start_next);
+                    current.getPcb().setEndofExec(end_next);
 
-                        //=====================================//
-                    } else {
-                        //==================================//
+                    next.getPcb().copy(drifter);
+                    next.getPcb().setStartofExec(start_current);
 
-                        //==================================//
-                        current = next;
-                        next = next.getNext();
-                    }
-                    System.out.println("the sort out ");
-                    printQueue();
+                    next.getPcb().setEndofExec(end_current);
+
+                } // if i have a redudnt  p0 p0' p1 at same time 
+                else if (next_arrival == current_arrival
+                        && current.getPcb().getPriority() < next.getPcb().getPriority()) {
+
+                    // trade //
+                    drifter.copy(current.getPcb());
+                    current.getPcb().copy(next.getPcb());
+
+                    current.getPcb().setStartofExec(start_next);
+                    current.getPcb().setEndofExec(end_next);
+
+                    next.getPcb().copy(drifter);
+                    next.getPcb().setStartofExec(start_current);
+
+                    next.getPcb().setEndofExec(end_current);
+                    //==================================//
+                    // change the arrival time depending on the shift  only on premum
+
+                    //====================================//
+                } // the 2 are premum
+                else if (next_arrival == current_arrival
+                        && current.getPcb().getPriority() > next.getPcb().getPriority()) {
+
+                } else if (next_arrival == current_arrival
+                        && current.getPcb().getPriority() < 0 && next.getPcb().getPriority() < 0) {
+
                 }
+                //if i have p0 p0' p1 
+                System.out.println("my queue  on arrival");
+                my_queue.printQueue();
+                System.out.println("============================");
 
+                current = current.getNext();
+                next = current.getNext();
             }
 
         }
-
     }
 
-    public float time(int mode) {
+    public void sort_2(int mode) {
+
+        System.out.println("my queue  on arrival final ");
+        my_queue.printQueue();
+        System.out.println("============================");
+
+        // i am sure it's sorted 
+        // so all i need is to get the first element and it's arrival time is my start xD
+        //======= fixed area =======//
+        for (int i = 0; i < node_number; i++) {
+            int run = 0;
+            run = my_queue.getHead().getPcb().getArrivalTime() + RR_time;// the first node is the start of my code and run = the [  ]
+
+            Node current = my_queue.getHead(); // current point at first node
+
+            Node next = current.getNext();  // next point at second node 
+            next.getPcb().setStartofExec(run);
+            next.getPcb().setEndofExec(run + RR_time);
+            run = run + RR_time;
+            current = next;// now next and current point at the second node
+            next = current.getNext();// next point at the third  node
+            while (current != null && next != null)// because the next we made with out check first time only
+            {
+
+                PCB drifter = new PCB(false);
+
+                int start_current = current.getPcb().getStartofExec();
+                int end_current = current.getPcb().getStartofExec() + RR_time;
+                int start_next = next.getPcb().getStartofExec();
+                int end_next = next.getPcb().getStartofExec() + RR_time;
+
+                // iterate till the end 
+                int current_arrival = current.getPcb().getArrivalTime();
+                int next_arrival = next.getPcb().getArrivalTime();
+
+                if (current_arrival < next_arrival && current.getPcb().getPriority() < next.getPcb().getPriority()) {
+                    if (run > next_arrival) {
+                        // trade //
+                        drifter.copy(current.getPcb());
+                        current.getPcb().copy(next.getPcb());
+
+                        current.getPcb().setStartofExec(start_next);
+                        current.getPcb().setEndofExec(end_next);
+                        // the third node will get the second exe time 
+                        // and the third will get the end of the second 
+                        next.getPcb().copy(drifter);
+
+                        //-----------------------------------//
+                        next.getPcb().setStartofExec(start_next + RR_time);
+
+                        next.getPcb().setEndofExec(end_next + RR_time);
+
+                    }
+
+                } else {
+                    next.getPcb().setStartofExec(run);
+                    next.getPcb().setEndofExec(run + RR_time);
+                }
+                //======================================================//
+                //
+                run = run + RR_time;
+                System.out.println("my queue  on exe");
+                my_queue.printQueue();
+                System.out.println("============================");
+                current = current.getNext();
+                next = current.getNext();
+            }
+        }
+    }
+    // claclulat turn around time //
+
+    public void time_confg() {
         // i am sure it's sorted 
         // so all i need is to get the first element and it's arrival time is my start xD
 
@@ -232,7 +325,7 @@ public class RoundRobin extends Queue implements ReadyQueue {
 
                 //========= fixed area ==========//
                 Node header;
-                header =my_queue.getHead() ;
+                header = my_queue.getHead();
                 boolean time_diff_bool = true;
 
                 //======================================//
@@ -276,12 +369,6 @@ public class RoundRobin extends Queue implements ReadyQueue {
 
             run_time += brust_time;
 
-            System.out.println("==============");
-            System.out.println("start = " + current.getPcb().getStartofExec());
-            System.out.println("RUN = " + run_time);
-            System.out.println("end = " + current.getPcb().getEndofExec());
-
-            System.out.println("==============");
             current = current.getNext();
 
         }
@@ -303,34 +390,11 @@ public class RoundRobin extends Queue implements ReadyQueue {
         //================================//
 
         // system out area //
-        System.out.println(" waiting_time final = " + waiting_time);
-        System.out.println(" turn around final = " + turn_around_time);
-        System.out.println(" number_of_pcb = " + number_of_pcb);
-        System.out.println(" avrage turn around final = " + avrage_turn_around_time);
-
-        switch (mode) {
-            case 0:
-                final_value = turn_around_time;
-                break;
-            case 1:
-                final_value = waiting_time;
-                break;
-            case 2:
-                final_value = avrage_turn_around_time;
-                break;
-            case 3:
-                final_value = avrage_waiting_time;
-                break;
-
-        }
-
-        return final_value;
+        turn_around_avr = avrage_turn_around_time;
+        waiting_avr = avrage_waiting_time;
     }
-    
-    
-    
-    
-      public float time2(int mode) {
+
+    public float time2(int mode) {
         // i am sure it's sorted 
         // so all i need is to get the first element and it's arrival time is my start xD
         System.out.println("-------------------------");
@@ -343,33 +407,29 @@ public class RoundRobin extends Queue implements ReadyQueue {
         int brust_time = 0;
         int ideal_time = 0;
         int start_of_exe = 0;
-       
-        int segma_arrival=0;
+
+        int segma_arrival = 0;
         float number_of_pcb = node_number;
         float turn_around_time = 0;
         float avrage_turn_around_time = 0;
         float waiting_time = 0;
         float avrage_waiting_time = 0;
-       
+
         Node current = my_queue.getHead();
 
         //======================================================//
         for (int i = 0; i < node_number; i++) {
-         
-            // fixed area //
 
+            // fixed area //
             brust_time = current.getPcb().getBurstTime();
             arrival_time = current.getPcb().getArrivalTime();
-            if(current.getPcb().getPriority()>=0)
-            {   
-                      
-                 segma_arrival+=arrival_time;  
+            if (current.getPcb().getPriority() >= 0) {
+
+                segma_arrival += arrival_time;
+            } else {
+                number_of_pcb--;
             }
-            else
-            {
-            number_of_pcb--;    
-            }
-           
+
             if (i == 0) {
 
                 //===============================//
@@ -384,11 +444,11 @@ public class RoundRobin extends Queue implements ReadyQueue {
 
             } // if not the first time 
             else {
-               
+
                 //================set ==========================//
                 current.getPcb().setStartofExec(end_of_exe);// my start is the end of the  first
                 if (arrival_time > end_of_exe) {
-                    ideal_time+=arrival_time- end_of_exe;
+                    ideal_time += arrival_time - end_of_exe;
                     current.getPcb().setStartofExec(arrival_time);// my start is the arrival
                 }
                 //=============== fixed area ====================//
@@ -404,28 +464,20 @@ public class RoundRobin extends Queue implements ReadyQueue {
 
             // after the if and else 
             //===================end of the for loop ====================//
-          
-
             run_time += brust_time;
 
-         
-          
             current = current.getNext();
 
         }
 
         // claclulat turn around time //
-       
-         System.out.println( "end_of_exe "+ end_of_exe);
-         System.out.println( "segma_arrival "+ segma_arrival);
-         System.out.println( "ideal_time "+ ideal_time);
-            turn_around_time =end_of_exe-segma_arrival+ideal_time;
-
-        
+        System.out.println("end_of_exe " + end_of_exe);
+        System.out.println("segma_arrival " + segma_arrival);
+        System.out.println("ideal_time " + ideal_time);
+        turn_around_time = end_of_exe - segma_arrival + ideal_time;
 
         // final area //
         //=================================//
-       
         waiting_time += turn_around_time - run_time;
         //=================================//
         avrage_turn_around_time = turn_around_time / number_of_pcb;
@@ -433,7 +485,6 @@ public class RoundRobin extends Queue implements ReadyQueue {
         //================================//
 
         // system out area //
-        
         switch (mode) {
             case 0:
                 final_value = turn_around_time;
